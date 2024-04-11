de490eaf6a7fc9c1bf22bbe85ebcb3e14b6e6d25
Miguel Lopes <Miguelfarialopes2000@gmail.com>
2024-04-11 18:42:59 +0100

    API
    Adicionada API com com 9 sensores:
    4 sensores sismicos
    2 sensores nas portas (principal e traseira)
    1 sensor de humidade
    1 sensor de fumo
    1 sensor de movimento
    
    Cada sensor tem 4 ficheiros txt que correspondem a:
    Valor-> Estado ou valor do sensor
    Data -> A data da ultima atualização do sensor
    Log -> Histórico de sensores
    Nome -> Nome do sensor

---
+++ api/api.php
@@ -0,0 +1,46 @@
+<?php
+
+header('Content-Type: text/html; charset=utf-8');
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){  
+
+    echo "recebi um POST";
+
+    echo $_POST['valor'];
+    
+file_put_contents("files/".$_POST['nome']."/valor.txt", $_POST['valor']);
+
+file_put_contents("files/".$_POST['nome']."/nome.txt", $_POST['nome']);
+
+file_put_contents("files/".$_POST['nome']."/data.txt", $_POST['data']);
+
+file_put_contents("files/".$_POST['nome']."/log.txt",$_POST['data'] ,PHP_EOL . $_POST['valor']);
+
+/*
+file_put_contents("files/".$_POST['nome']."/data.txt", PHP_EOL . date("Y-m-d H:i:s"));
+
+file_put_contents("files/".$_POST['nome']."/log.txt", PHP_EOL . date("Y-m-d H:i:s"), $_POST['valor']);
+*/
+}
+// SE (Recebido um GET) ENTÃO //esta linha já foi feita
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    // SE (Array GET tem um elemento "nome") ENTÃO
+    if (isset($_GET['nome'])) {
+        // Imprime no ecrã o conteúdo do ficheiro "valor.txt" correspondente sensor pedido (nome).
+        $nome = $_GET['nome'];
+        $file = file_get_contents("valor.txt");
+        $lines = explode("\n", $file);
+        foreach ($lines as $line) {
+            $parts = explode(" ", $line);
+            if ($parts[0] == $nome) {
+                echo $parts[1];
+                break;
+            }
+        }
+    } else {
+        // Imprime erro no ecrã: "faltam parametros no GET".
+        echo "faltam parametros no GET";
+    }
+}
+
+
